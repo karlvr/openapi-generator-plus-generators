6861b776fded7e1fd0574c2b62219307e4f11b71
Karl von Randow <karl@xk72.com>
2020-03-31 10:09:01 +1300

    generator-common: fix defaultOperationName

--- packages/generator-common/src/index.ts
+++ packages/generator-common/src/index.ts
@@ -5,6 +5,6 @@ export function defaultOperationName(path: string, method: string): string {
 	/* Remove path variables from the path */
 	const sanitisedPath = path.replace(/\{[^}]*\}/g, '')
 	const combined = `${method.toLocaleLowerCase()}_${sanitisedPath}`
-	const sanitizedCombined = combined.replace(/^a-zA-Z0-9/g, '_')
+	const sanitizedCombined = combined.replace(/[^a-zA-Z0-9]/g, '_').replace(/_+$/, '')
 	return camelCase(sanitizedCombined)
 }
