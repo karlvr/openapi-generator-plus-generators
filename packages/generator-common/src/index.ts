0d0f94379230e76cfa52eb0f9f66c0d847fb1346
Karl von Randow <karl@xk72.com>
2020-04-01 21:00:20 +1300

    generator-common: move the common generator to core
    generator-common is useless right now, but we’ll find something useful for it to do

--- packages/generator-common/src/index.ts
+++ packages/generator-common/src/index.ts
@@ -1,21 +1,8 @@
-import { camelCase, CodegenGenerator, CodegenOptions } from '@openapi-generator-plus/core'
-import pluralize from 'pluralize'
+import { CodegenGenerator, CodegenOptions } from '@openapi-generator-plus/core'
 
-/** Create a default operation name for operations that lack an operationId */
-export function defaultOperationName(path: string, method: string): string {
-	/* Remove path variables from the path */
-	const sanitisedPath = path.replace(/\{[^}]*\}/g, '')
-	const combined = `${method.toLocaleLowerCase()}_${sanitisedPath}`
-	const sanitizedCombined = combined.replace(/[^a-zA-Z0-9]/g, '_').replace(/_+$/, '')
-	return camelCase(sanitizedCombined)
-}
-
-export function commonGenerator<O extends CodegenOptions>(): Pick<CodegenGenerator<O>, 'toIteratedModelName' | 'toModelNameFromPropertyName' | 'toOperationName'> {
+// Change to Pick<CodegenGenerator<O>, 'NAME'> when we implement something here
+export function commonGenerator<O extends CodegenOptions>(): Partial<CodegenGenerator<O>> {
 	return {
-		toIteratedModelName: (name, _, iteration) => `${name}${iteration}`,
-		toModelNameFromPropertyName: (name, state) => {
-			return state.generator.toClassName(pluralize.singular(name), state)
-		},
-		toOperationName: defaultOperationName,
+		
 	}
 }
