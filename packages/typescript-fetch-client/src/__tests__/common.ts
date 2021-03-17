0573a1b10523e461ad5ced63369b96c948d40c6f
Karl von Randow <karl@xk72.com>
2021-03-17 13:30:48 +1300

    typescript-fetch-client: testing: improve error reporting

--- packages/typescript-fetch-client/src/__tests__/common.ts
+++ packages/typescript-fetch-client/src/__tests__/common.ts
@@ -31,8 +31,8 @@ export async function compile(basePath: string): Promise<void> {
 	const result = program.emit()
 	if (result.emitSkipped && result.diagnostics.length) {
 		const diagnostics = result.diagnostics
-			.map(d => `${d.file ? path.basename(d.file.fileName) : ''}:${d.file && d.start !== undefined ? d.file.getLineAndCharacterOfPosition(d.start).line : '?'} ${d.messageText}`)
-		throw new Error(`Failed to compile. ${result.diagnostics.length} diagnostics:\n  ${diagnostics.join('\n  ')}`)
+			.map(d => `${d.file ? d.file.fileName : ''}:${d.file && d.start !== undefined ? d.file.getLineAndCharacterOfPosition(d.start).line : '?'} ${d.messageText}`)
+		throw new Error(`Failed to compile. ${diagnostics.length} diagnostics:\n  ${diagnostics.join('\n  ')}`)
 	} else if (result.diagnostics.length) {
 		for (const d of result.diagnostics) {
 			console.warn(d.messageText)
