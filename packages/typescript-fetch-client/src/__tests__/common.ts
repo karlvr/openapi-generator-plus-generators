652c2d39554c733d851bc73c0808e10b415b30a6
Karl von Randow <karl@xk72.com>
2021-09-03 10:19:14 +1200

    typescript-fetch-client: test using npm rather than typescript directly
    We need to test installing packages and compiling with the TypeScript options that are in our generated tsconfig.json

--- packages/typescript-fetch-client/src/__tests__/common.ts
+++ packages/typescript-fetch-client/src/__tests__/common.ts
@@ -1,12 +1,11 @@
-import ts from 'typescript'
-import glob from 'glob-promise'
 import path from 'path'
 import { CodegenConfig } from '@openapi-generator-plus/types'
 import { CodegenResult, createCodegenResult } from '@openapi-generator-plus/testing'
 import createGenerator from '..'
+import { exec } from 'child_process'
 
 export const DEFAULT_CONFIG: CodegenConfig = {
-	maven: {},
+	npm: {},
 }
 
 export async function prepare(spec: string, config?: CodegenConfig): Promise<CodegenResult> {
@@ -14,28 +13,19 @@ export async function prepare(spec: string, config?: CodegenConfig): Promise<Cod
 }
 
 export async function compile(basePath: string): Promise<void> {
-	const searchPath = path.join(basePath, '**/*.ts')
-	const paths = await glob(searchPath)
-	if (!paths.length) {
-		throw new Error('No files found to compile')
-	}
-	const program = ts.createProgram(paths, {
-		noEmitOnError: true,
-		noImplicitAny: true,
-		target: ts.ScriptTarget.ES2019,
-		module: ts.ModuleKind.CommonJS,
-		skipDefaultLibCheck: true,
-		skipLibCheck: true,
-		strict: true,
+	return new Promise(function(resolve, reject) {
+		exec(
+			'npm install', 
+			{
+				cwd: basePath,
+			},
+			function(error, stdout, stderr) {
+				if (error) {
+					reject(new Error(`${error.cmd || '<unknown>'} exited with code ${error.code || 'unknown'}:\n ${stdout || stderr}`))
+				} else {
+					resolve()
+				}
+			}
+		)
 	})
-	const result = program.emit()
-	if (result.emitSkipped && result.diagnostics.length) {
-		const diagnostics = result.diagnostics
-			.map(d => `${d.file ? d.file.fileName : ''}:${d.file && d.start !== undefined ? d.file.getLineAndCharacterOfPosition(d.start).line : '?'} ${d.messageText}`)
-		throw new Error(`Failed to compile. ${diagnostics.length} diagnostics:\n  ${diagnostics.join('\n  ')}`)
-	} else if (result.diagnostics.length) {
-		for (const d of result.diagnostics) {
-			console.warn(d.messageText)
-		}
-	}
 }
