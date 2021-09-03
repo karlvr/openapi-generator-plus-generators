f32445e58c8191ea0cf74097796a3a826e1a7f55
Karl von Randow <karl@xk72.com>
2021-09-03 12:16:51 +1200

    testing: increase test timeout to support GitHub actions

--- packages/java-jaxrs-server/jest.config.js
+++ packages/java-jaxrs-server/jest.config.js
@@ -8,5 +8,5 @@ module.exports = {
 	],
 	/* Only run files with test or spec in their filename, so we can have support files in __tests__ */
 	testRegex: '(\\.|/)(test|spec)\\.[jt]sx?$',
-	testTimeout: 60000,
+	testTimeout: 120000,
 }
