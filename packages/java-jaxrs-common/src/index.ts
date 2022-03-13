6ebd170589a311ae1b96b8c56bd97f19abd91670
Karl von Randow <karl@xk72.com>
2022-03-14 10:14:16 +1300

    java-jaxrs-common: Check property compatibility for inheritance: null values aren't compatible as we use java.util.Optional

--- packages/java-jaxrs-common/src/index.ts
+++ packages/java-jaxrs-common/src/index.ts
@@ -136,10 +136,11 @@ function createJavaLikeContext(context: JavaGeneratorContext): JavaLikeContext {
 export default function createGenerator(config: CodegenConfig, context: JavaGeneratorContext): Omit<CodegenGenerator, 'generatorType'> {
 	const generatorOptions = options(config, context)
 
+	const baseGenerator = context.baseGenerator(config, context)
 	const aCommonGenerator = commonGenerator(config, context)
 
 	return {
-		...context.baseGenerator(config, context),
+		...baseGenerator,
 		...aCommonGenerator,
 		...javaLikeGenerator(config, createJavaLikeContext(context)),
 		toLiteral: (value, options) => {
@@ -644,6 +645,20 @@ export default function createGenerator(config: CodegenConfig, context: JavaGene
 					}
 				}
 			}
+		},
+
+		checkPropertyCompatibility: (parentProp, childProp) => {
+			if (!baseGenerator.checkPropertyCompatibility(parentProp, childProp)) {
+				return false
+			}
+
+			/* Because in Java we use a java.util.Optional if a property is nullable, properties are not compatible
+			   if their nullability varies.
+			 */
+			if (!parentProp.nullable !== !childProp.nullable) {
+				return false
+			}
+			return true
 		},
 	}
 }
