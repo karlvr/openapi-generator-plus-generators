karlvr/openapi-generator-plus-generators

commit 73e5402bd45cead04afd37c4eedf9d2250f5d7b3

typescript-fetch-client2: Generate enums as disjunctions, but retain a supporting enum

packages/typescript-fetch-client2/src/index.ts
@@ -67,6 +67,14 @@ const createGenerator: CodegenGeneratorConstructor = (config, context) => {
 				return base.toNativeType(options)
 			}
 		},
+		toSuggestedSchemaName: (name, options) => {
+			if (options.schemaType === CodegenSchemaType.ENUM) {
+				/* We don't add a suffix to enums as we generate them as disjunctions */
+				return name
+			} else {
+				return base.toSuggestedSchemaName(name, options)
+			}
+		},
 		postProcessDocument: (doc) => {
 			for (const group of doc.groups) {
 				for (const op of group.operations) {

packages/typescript-fetch-client2/templates/modelEnum.hbs
@@ -1,6 +1,13 @@
 {{>frag/schemaDocumentation}}
-export enum {{className name}} {
+export type {{className name}} =
 {{#each enumValues}}
-	{{{name}}} = {{{literalValue}}}{{#hasMore}},{{/hasMore}}
+	{{{literalValue}}}{{#hasMore}} |{{/hasMore}}
 {{/each}}
+
+export namespace {{className name}} {
+	export enum Enum {
+	{{#each enumValues}}
+		{{{name}}} = {{{literalValue}}}{{#hasMore}},{{/hasMore}}
+	{{/each}}
+	}
 }

commit 57ed79a5f49c95007af80745c86a9c4efd650070

typescript: fix enum literals in discriminators
Regression in 73e5402bd45cead04afd37c4eedf9d2250f5d7b3

packages/typescript-common/src/index.ts
@@ -44,11 +44,13 @@ export interface TypeScriptGeneratorContext extends CodegenGeneratorContext {
 	additionalExportTemplates?: (outputPath: string, doc: CodegenDocument, hbs: typeof Handlebars, rootContext: Record<string, unknown>) => Promise<void>
 	defaultNpmOptions?: (config: CodegenConfig, defaultValue: NpmOptions) => NpmOptions
 	defaultTypeScriptOptions?: (config: CodegenConfig, defaultValue: TypeScriptOptions) => TypeScriptOptions
+	toEnumLiteral?: CodegenGenerator['toLiteral']
 }
 
 export function chainTypeScriptGeneratorContext(base: TypeScriptGeneratorContext, add: Partial<TypeScriptGeneratorContext>): TypeScriptGeneratorContext {
 	const result: TypeScriptGeneratorContext = {
 		...base,
+		...add,
 		loadAdditionalTemplates: async function(hbs) {
 			/* Load the additional first, so that earlier contexts in the chain have priority */
 			if (add.loadAdditionalTemplates) {
@@ -95,6 +97,13 @@ export function chainTypeScriptGeneratorContext(base: TypeScriptGeneratorContext
 				result = base.defaultTypeScriptOptions(config, result)
 			}
 			return result
+		},
+		toEnumLiteral(value, options) {
+			if (add.toEnumLiteral) {
+				return add.toEnumLiteral(value, options)
+			} else {
+				return `${options.nativeType.concreteType}.${result.generator().toEnumMemberName(String(value))}`
+			}
 		},
 	}
 	return result
@@ -240,8 +249,8 @@ export default function createGenerator(config: CodegenConfig, context: TypeScri
 
 			const { type, format, schemaType } = options
 
-			if (schemaType === CodegenSchemaType.ENUM) {
-				return `${options.nativeType.concreteType}.${context.generator().toEnumMemberName(String(value))}`
+			if (schemaType === CodegenSchemaType.ENUM && context.toEnumLiteral) {
+				return context.toEnumLiteral(value, options)
 			}
 
 			switch (type) {

packages/typescript-fetch-client2/src/index.ts
@@ -23,6 +23,13 @@ const createGenerator: CodegenGeneratorConstructor = (config, context) => {
 			target: 'ES5',
 			libs: ['$target', 'DOM'],
 		}),
+		toEnumLiteral(value, options) {
+			/* We just want to output the literal string value as we change enums to disjunctions */
+			return myContext.generator().toLiteral(value, {
+				...options,
+				schemaType: CodegenSchemaType.STRING,
+			})
+		},
 	})
 
 	const generatorOptions: CodegenOptionsTypeScriptFetchClient = {