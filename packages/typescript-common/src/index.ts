karlvr/openapi-generator-plus-generators

commit 57ed79a5f49c95007af80745c86a9c4efd650070

typescript: fix enum literals in discriminators
Regression in 73e5402bd45cead04afd37c4eedf9d2250f5d7b3

packages/typescript-common/src/index.ts
@@ -44,11 +44,13 @@ export interface TypeScriptGeneratorContext extends CodegenGeneratorContext {
 	additionalExportTemplates?: (outputPath: string, doc: CodegenDocument, hbs: typeof Handlebars, rootContext: Record<string, unknown>) => Promise<void>
 	defaultNpmOptions?: (config: CodegenConfig, defaultValue: NpmOptions) => NpmOptions
 	defaultTypeScriptOptions?: (config: CodegenConfig, defaultValue: TypeScriptOptions) => TypeScriptOptions
+	toEnumLiteral?: CodegenGenerator['toLiteral']
 }
 
 export function chainTypeScriptGeneratorContext(base: TypeScriptGeneratorContext, add: Partial<TypeScriptGeneratorContext>): TypeScriptGeneratorContext {
 	const result: TypeScriptGeneratorContext = {
 		...base,
+		...add,
 		loadAdditionalTemplates: async function(hbs) {
 			/* Load the additional first, so that earlier contexts in the chain have priority */
 			if (add.loadAdditionalTemplates) {
@@ -95,6 +97,13 @@ export function chainTypeScriptGeneratorContext(base: TypeScriptGeneratorContext
 				result = base.defaultTypeScriptOptions(config, result)
 			}
 			return result
+		},
+		toEnumLiteral(value, options) {
+			if (add.toEnumLiteral) {
+				return add.toEnumLiteral(value, options)
+			} else {
+				return `${options.nativeType.concreteType}.${result.generator().toEnumMemberName(String(value))}`
+			}
 		},
 	}
 	return result
@@ -240,8 +249,8 @@ export default function createGenerator(config: CodegenConfig, context: TypeScri
 
 			const { type, format, schemaType } = options
 
-			if (schemaType === CodegenSchemaType.ENUM) {
-				return `${options.nativeType.concreteType}.${context.generator().toEnumMemberName(String(value))}`
+			if (schemaType === CodegenSchemaType.ENUM && context.toEnumLiteral) {
+				return context.toEnumLiteral(value, options)
 			}
 
 			switch (type) {

packages/typescript-fetch-client2/src/index.ts
@@ -23,6 +23,13 @@ const createGenerator: CodegenGeneratorConstructor = (config, context) => {
 			target: 'ES5',
 			libs: ['$target', 'DOM'],
 		}),
+		toEnumLiteral(value, options) {
+			/* We just want to output the literal string value as we change enums to disjunctions */
+			return myContext.generator().toLiteral(value, {
+				...options,
+				schemaType: CodegenSchemaType.STRING,
+			})
+		},
 	})
 
 	const generatorOptions: CodegenOptionsTypeScriptFetchClient = {

commit a5561e1a2ffb8636fc24af7204158ad4c3a22651

typescript: Fix enum literals in typescript-express-example-server-generator
regression from 57ed79a5f49c95007af80745c86a9c4efd650070

packages/typescript-common/src/index.ts
@@ -98,13 +98,6 @@ export function chainTypeScriptGeneratorContext(base: TypeScriptGeneratorContext
 			}
 			return result
 		},
-		toEnumLiteral(value, options) {
-			if (add.toEnumLiteral) {
-				return add.toEnumLiteral(value, options)
-			} else {
-				return `${options.nativeType.concreteType}.${result.generator().toEnumMemberName(String(value))}`
-			}
-		},
 	}
 	return result
 }
@@ -249,8 +242,12 @@ export default function createGenerator(config: CodegenConfig, context: TypeScri
 
 			const { type, format, schemaType } = options
 
-			if (schemaType === CodegenSchemaType.ENUM && context.toEnumLiteral) {
-				return context.toEnumLiteral(value, options)
+			if (schemaType === CodegenSchemaType.ENUM) {
+				if (context.toEnumLiteral) {
+					return context.toEnumLiteral(value, options)
+				} else {
+					return `${options.nativeType.concreteType}.${context.generator().toEnumMemberName(String(value))}`
+				}
 			}
 
 			switch (type) {

packages/typescript-express-example-server/src/index.ts
@@ -1,11 +1,10 @@
 import { CodegenGeneratorConstructor, CodegenGeneratorType } from '@openapi-generator-plus/types'
 import path from 'path'
 import { loadTemplates, emit } from '@openapi-generator-plus/handlebars-templates'
-import typescriptGenerator, { options as typescriptGeneratorOptions, TypeScriptGeneratorContext } from '@openapi-generator-plus/typescript-generator-common'
+import typescriptGenerator, { options as typescriptGeneratorOptions, TypeScriptGeneratorContext, chainTypeScriptGeneratorContext } from '@openapi-generator-plus/typescript-generator-common'
 
 const createGenerator: CodegenGeneratorConstructor<TypeScriptGeneratorContext> = (config, context) => {
-	const myContext: TypeScriptGeneratorContext = {
-		...context,
+	const myContext: TypeScriptGeneratorContext = chainTypeScriptGeneratorContext(context, {
 		loadAdditionalTemplates: async(hbs) => {
 			/* Convert an operation path to an express path */
 			hbs.registerHelper('expressPath', function(path: string) {
@@ -30,7 +29,7 @@ const createGenerator: CodegenGeneratorConstructor<TypeScriptGeneratorContext> =
 			target: 'ES2015',
 			libs: ['$target', 'DOM'],
 		}),
-	}
+	})
 
 	const generatorOptions = typescriptGeneratorOptions(config, myContext)
 